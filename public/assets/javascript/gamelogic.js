d9dbf6c2dfe565b6feb4828c9138d6549963dda2
Jorge G <jogoram428@gmail.com>
2019-03-09 04:11:18 -0700

    game logic

--- public/assets/javascript/gamelogic.js
+++ public/assets/javascript/gamelogic.js
@@ -1,72 +1,156 @@
-let question = document.getElementById('question');
-let answerA = document.getElementById('answer_a');
-let answerB = document.getElementById('answer_b');
-let answerC = document.getElementById('answer_c');
-let answerD = document.getElementById('answer_d');
-console.log("hello gamelogic has run");
-
-let currentRight = 0;
-let currentWrong = 0;
-let playerArray;
-
 let socket = io();
-console.log("gamelogic.js ran");
-socket.on("welcome", function(data) {
-    console.log(data);
+
+var Game = {
+    players: [],
+    ready: false,
+    question: 1,
+    start: function() {
+        Game.ready = true;
+        console.log(Game.players);
+        // Game.showPlayers();
+    },
+    stop: function () {
+        Game.ready = false;
+        Game.question = 1;
+        Player.score = 0;
+        // $("#waiting").show();
+        // $(".player-info").show();
+        // $(".game").hide();
+        // $(".intro").hide();
+        // $(".results").hide();
+    },
+    showPlayers: function() {
+        $(`#player_name`).html(`${Game.players[0].username}`);
+        $("#player1").attr("src",`${Game.players[0].imageURL}`);
+        $(`#player_right_count`).html(`${Game.players[0].score}`);
+        $(`#player2_name`).html(`${Game.players[1].username}`);
+        $("#player2").attr("src",`${Game.players[1].imageURL}`);
+        $(`#player2_right_count`).html(`${Game.players[1].score}`);
+    }
+}
+
+var Player = {
+    id: '',
+    userName: '',
+    score: 0,
+    join: function () {
+        Game.players.push(this);
+        socket.emit("addPlayer", this);
+    },
+    addPoints: function() {
+        Player.score = Player.score + 5;
+        socket.emit("logPoints", this);
+    },
+    deductPoints: function() {
+        Player.score = Player.score - 3;
+        socket.emit("logPoints", this);
+    }
+};
+
+socket.on('ready', function(ready) {
+    if (ready) {
+        console.log('Game has 2 players, ready to start!');
+        Game.start();
+    } else {
+        console.log('1 or more players have left, resetting game...');
+        Game.stop();
+    }
 });
 
-socket.on('playerArray', function(data) {
-    playerArray = data;
-    console.log(playerArray);
+socket.on('socketID', function(socketid) {
+    for(let i = 0; i < Game.players.length; i++){
+        if(Game.players[i].id == socketid){
+            Player.id = Game.players[i].id;
+            Player.userName = Game.players[i].username;
+            console.log(Player.userName);
+        }
+    }
+    console.log(Game.players);
+    // Game.players = players;
+});
+
+socket.on('players', function(players) {
+    // for(let i = 0; i < players.length; i++){
+    //     Game.players.push(players[i]);
+    // }
+    // console.log(Game.players);
+    Game.players = players;
 });
 
 
-socket.on("questions", function(data) {
-    console.log(data);
-    $(`#button_b`).css("background-color", "orange");
-    $(`.submit-answer`).prop('disabled', false);
-    question.innerHTML = `<p><em>${data.question}</em></p>`;
-    answerA.innerHTML = `<p><em>${data.answer}</em></p>`;
-    answerB.innerHTML = `<p><em>${data.wrongOne}</em></p>`;
-    answerC.innerHTML = `<p><em>${data.wrongTwo}</em></p>`;
-    answerD.innerHTML = `<p><em>${data.wrongThree}</em></p>`;
+
+
+
+
+// let question = document.getElementById('question');
+// let answerA = document.getElementById('answer_a');
+// let answerB = document.getElementById('answer_b');
+// let answerC = document.getElementById('answer_c');
+// let answerD = document.getElementById('answer_d');
+// console.log("hello gamelogic has run");
+
+// let currentRight = 0;
+// let currentWrong = 0;
+// let playerArray;
+
+
+// console.log("gamelogic.js ran");
+// socket.on("welcome", function(data) {
+//     console.log(data);
+// });
+
+// socket.on('playerArray', function(data) {
+//     playerArray = data;
+//     console.log(playerArray);
+// });
+
+
+// socket.on("questions", function(data) {
+//     console.log(data);
+//     $(`#button_b`).css("background-color", "orange");
+//     $(`.submit-answer`).prop('disabled', false);
+//     question.innerHTML = `<p><em>${data.question}</em></p>`;
+//     answerA.innerHTML = `<p><em>${data.answer}</em></p>`;
+//     answerB.innerHTML = `<p><em>${data.wrongOne}</em></p>`;
+//     answerC.innerHTML = `<p><em>${data.wrongTwo}</em></p>`;
+//     answerD.innerHTML = `<p><em>${data.wrongThree}</em></p>`;
     
-});
+// });
 
-socket.on('right', function() {
-    currentRight += 1;
-    console.log(currentRight);
-    $(`#answer_a`).empty();
-    $(`#answer_b`).html(`Correct!`);
-    $(`#button_b`).css("background-color", "green");
-    $(`.submit-answer`).prop('disabled', true);
-    $(`#answer_c`).empty();
-    $(`#answer_d`).empty();
-    $(`#player_right_count`).html(`RIGHT: ${currentRight}`);
-    $(`#player_score`).html(`POINTS: `, currentRight);
-    $(`#opponent_score`).html(`POINTS: `, currentRight);
-    // $(`#opponent_right_count`).html(`Right: ${currentRight}`);
-});
+// socket.on('right', function() {
+//     currentRight += 1;
+//     console.log(currentRight);
+//     $(`#answer_a`).empty();
+//     $(`#answer_b`).html(`Correct!`);
+//     $(`#button_b`).css("background-color", "green");
+//     $(`.submit-answer`).prop('disabled', true);
+//     $(`#answer_c`).empty();
+//     $(`#answer_d`).empty();
+//     $(`#player_right_count`).html(`RIGHT: ${currentRight}`);
+//     $(`#player_score`).html(`POINTS: `, currentRight);
+//     $(`#opponent_score`).html(`POINTS: `, currentRight);
+//     // $(`#opponent_right_count`).html(`Right: ${currentRight}`);
+// });
 
-socket.on('wrong', function() {
-    currentWrong += 1;
-    console.log(currentWrong);
-    $(`#answer_a`).empty();
-    $(`#answer_b`).html(`Wrong!`);
-    $(`#button_b`).css("background-color", "red");
-    $(`.submit-answer`).prop('disabled', true);
-    $(`#answer_c`).empty();
-    $(`#answer_d`).empty();
-    $(`#player_wrong_count`).html(`WRONG: ${currentWrong}`);
-    // $(`#opponent_wrong_count`).html(`Wrong: ${currentWrong}`);
-});
+// socket.on('wrong', function() {
+//     currentWrong += 1;
+//     console.log(currentWrong);
+//     $(`#answer_a`).empty();
+//     $(`#answer_b`).html(`Wrong!`);
+//     $(`#button_b`).css("background-color", "red");
+//     $(`.submit-answer`).prop('disabled', true);
+//     $(`#answer_c`).empty();
+//     $(`#answer_d`).empty();
+//     $(`#player_wrong_count`).html(`WRONG: ${currentWrong}`);
+//     // $(`#opponent_wrong_count`).html(`Wrong: ${currentWrong}`);
+// });
 
 
-$(".submit-answer").on("click", function(event) {
-    event.preventDefault();
+// $(".submit-answer").on("click", function(event) {
+//     event.preventDefault();
 
-    let userGuess = $(`input[name='answerBtn']:checked`).val();
-    console.log(userGuess);
+//     let userGuess = $(`input[name='answerBtn']:checked`).val();
+//     console.log(userGuess);
     
-    socket.emit('answer', userGuess);
-});        
+//     socket.emit('answer', userGuess);
+// });        
--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 const app = express();
 const server = require("http").createServer(app);
-// const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/config/config.json')[env];
-
 const session = require('express-session');
 const connect = require("connect");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const MySQLStore = require('express-mysql-session')(session);
 const passport = require("passport")
 const io = require("socket.io")(server);
 const passportSocketIo = require("passport.socketio");
@@ -24,20 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
-//used to determine if user is logged in
-// const options = {
-//     host: config.host,
-//     port: config.port,
-//     user: config.username,
-//     password: config.password,
-//     database: config.database
-// };
-// const sessionStore = new MySQLStore(options);
-// console.log(datab.Sequelize);
-// connect().use(connect.session({
-//     store: new SequelizeStore(db)
-//     , secret: 'itsasecret'
-// }));
+
 let myStore = new SequelizeStore({
     db: datab.sequelize
 })
@@ -47,18 +30,9 @@ app.use(session({
     store: myStore,
     saveUninitialized: false,
     proxy: true,
-    cookie: {secure: true}
+    // cookie: {secure: true}
 }))
 
-// myStore.sync();
-// app.use(session({
-// 	secret: 'itsasecret',
-//     resave: false,
-//     store: sessionStore,
-//     saveUninitialized: false,
-//     // cookie: {secure: true}
-// }));
-//set up passport.js
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -70,7 +44,6 @@ app.use(function(req, res, next) {
 // const cookieParser = require("cookie-parser");
 // app.use(cookieParser());
 io.use(passportSocketIo.authorize({
-    // cookieParser: cookieParser,       // the same middleware you registrer in express
     key:          'connect.sid',       // the name of the cookie where express/connect stores its session_id
     secret:       'itsasecret',    // the session_secret to parse the cookie
     store:        myStore,        // we NEED to use a sessionstore. no memorystore please
@@ -83,17 +56,12 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// app.get("/", function(req, res) {
-//     res.sendFile(__dirname + "/public/index.html");
-// });
+
 require("./routes/html-routes")(app);
 require("./routes/login")(app);
 require("./controllers/orangezone-controller")(app);
 
 require("./controllers/server_game_controller")(io);
-
-
-
 
 // Requiring our models for syncing
 var db = require("./models");
